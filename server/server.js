a06b8855ebf20f91483981289992a6e9bc6ec189
netlooker <czarny@data.pl>
2017-08-13 20:28:51 +0200

    Adding the socket.io + http server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-const port = process.env.PORT || 3000;
+const socketIO = require('socket.io');
 
+const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port: ${port}`);
 });
