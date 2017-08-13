ff514b616f22c56edf893a8b8344d5e4c964f99b
netlooker <czarny@data.pl>
2017-08-13 22:56:12 +0200

    Adding the body-parser package and setting out the webhooks endpoint

--- server/server.js
+++ server/server.js
@@ -2,11 +2,20 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+app.use(bodyParser.json());
+
+app.post('/webhooks', (req, res) => {
+    if (req.body.operation === 'play') {
+        console.log(req.body);
+    }
+});
+
 var server = http.createServer(app);
 var io = socketIO(server);
 
