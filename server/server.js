7e54871e59eaaa44671cc32a3ac4e9dd856cc6d2
netlooker <czarny@data.pl>
2017-08-15 00:33:35 +0200

    Removing fs lib

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
