8928067d1cffb13d45d114850e2ffabac7e98172
netlooker <czarny@data.pl>
2017-08-15 00:24:17 +0200

    Addapting code to play sound on the github commit event

--- server/server.js
+++ server/server.js
@@ -12,9 +12,7 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/webhooks', (req, res) => {
-    fs.writeFileSync(`${publicPath}/data.json`, JSON.stringify(req.body));
-    if (req.body.operation === 'play') {
-        console.log(req.body);
+    if (req.body.commits.length > 0) {
         io.emit('playSound');
     }
 });
